0f93957e7c7ba746ee463402722f57ddc9083897
muchokiesther <muchokiesther8@gmail.com>
2023-05-10 17:00:52 +0300

    users endpoint

--- Databases/storedprocedure/usersprocedures/spadduser.sql
+++ Databases/storedprocedure/usersprocedures/spadduser.sql
@@ -1,5 +1,11 @@
 -- add user
-CREATE OR ALTER PROCEDURE addUser(@id INT , @username VARCHAR, @fullname VARCHAR, @email VARCHAR,@phonenumber VARCHAR, @password VARCHAR)
+CREATE OR ALTER PROCEDURE addUser(
+	@id VARCHAR(200), 
+	@username VARCHAR(200), 
+	@fullname VARCHAR(200), 
+	@email VARCHAR(200),
+	@phonenumber VARCHAR(200), 
+	@password VARCHAR(200))
 AS 
 BEGIN
 INSERT INTO Users(id,userName,fullName,email,phoneNumber,password) VALUES (@id, @username, @fullname, @email, @phonenumber, @password)
